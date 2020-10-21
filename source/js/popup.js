c21b5150d32e9a824644e694f2732fdeb88bf640
Rubylnic <ksenia.bolshako@mail.ru>
2020-10-22 00:46:49 +0300

    js

--- source/js/popup.js
+++ source/js/popup.js
@@ -30,22 +30,4 @@
   btnClose.addEventListener('click', closeModal);
   document.addEventListener('keydown', escPressHandler);
   btnOpen.addEventListener('keydown', enterPressHandler)
-
-  // accordeon
-  var headings = document.querySelectorAll('.footer__upper-heading');
-  var links = document.querySelectorAll('.footer__upper-list');
-  links.forEach((element) => element.classList.add('footer__upper-list--closed'));
-  headings.forEach((element) => element.classList.add('footer__upper-heading--closed'));
-
-  headings.forEach((element) => {
-    element.addEventListener('click', function() {
-      if (element.nextElementSibling.classList.contains('footer__upper-list--closed')) {
-        element.classList.remove('footer__upper-heading--closed');
-        element.nextElementSibling.classList.remove('footer__upper-list--closed');
-      } else {
-        element.classList.add('footer__upper-heading--closed');
-        element.nextElementSibling.classList.add('footer__upper-list--closed')
-      };
-    })
-  });
 })();
