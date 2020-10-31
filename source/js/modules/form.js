03986e43cd8c09d1fcb9be085778595b19d93e44
Rubylnic <ksenia.bolshako@mail.ru>
2020-10-31 10:26:08 +0300

    'imask'

--- source/js/modules/form.js
+++ source/js/modules/form.js
@@ -34,5 +34,11 @@
       }
     }
   });
+  import IMask from 'imask';
+  var element = document.getElementById('selector');
+var maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+var mask = IMask(element, maskOptions);
 
 })();
