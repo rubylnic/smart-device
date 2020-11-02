b12b90c20b0e74ffc0a900eafc37f63c9f0c8b01
Rubylnic <ksenia.bolshako@mail.ru>
2020-11-02 14:51:46 +0300

    'imask'

--- build/js/main.js
+++ build/js/main.js
@@ -78,13 +78,15 @@
       }
     }
   });
+})();
 
+'use strict';
+(function () {
   var phoneMask = IMask(
     document.getElementById('questions-tel'), {
       mask: '+{7}(000)000-00-00'
     });
-
-})();
+})()
 
 'use strict';
 (function () {
